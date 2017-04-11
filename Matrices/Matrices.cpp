ff5a3a2d3f6d22fd51fcd384cb90a5d68e618186
Woo728 <kewcjzz113@naver.com>
2017-04-11 23:23:23 +0900

    Matrices10 추가

--- Matrices/Matrices.cpp
+++ Matrices/Matrices.cpp
@@ -625,6 +625,49 @@ VOID SetupMatrices09()
 	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
 }
 
+VOID SetupMatrices10()
+{
+	// For our world matrix, we will just rotate the object about the y-axis.
+	D3DXMATRIXA16 matWorld01;
+	D3DXMATRIXA16 matWorld02;
+	D3DXMATRIXA16 matWorld03;
+
+	// Set up the rotation matrix to generate 1 full rotation (2*PI radians) 
+	// every 1000 ms. To avoid the loss of precision inherent in very high 
+	// floating point numbers, the system time is modulated by the rotation 
+	// period before conversion to a radian angle.
+
+	D3DXMatrixTranslation(&matWorld01, 0.0f, 0.0f, B);
+	D3DXMatrixRotationY(&matWorld02, -A);
+	D3DXMatrixRotationZ(&matWorld03, A);
+	D3DXMatrixMultiply(&matWorld02, &matWorld02, &matWorld03);
+	D3DXMatrixMultiply(&matWorld01, &matWorld01, &matWorld02);
+
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld01);
+
+
+	// Set up our view matrix. A view matrix can be defined given an eye point,
+	// a point to lookat, and a direction for which way is up. Here, we set the
+	// eye five units back along the z-axis and up three units, look at the
+	// origin, and define "up" to be in the y-direction.
+	D3DXVECTOR3 vEyePt(0.0f, 3.0f, -5.0f);
+	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);
+	D3DXMATRIXA16 matView;
+	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);
+	g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
+
+	// For the projection matrix, we set up a perspective transform (which
+	// transforms geometry from 3D view space to 2D viewport space, with
+	// a perspective divide making objects smaller in the distance). To build
+	// a perpsective transform, we need the field of view (1/4 pi is common),
+	// the aspect ratio, and the near and far clipping planes (which define at
+	// what distances geometry should be no longer be rendered).
+	D3DXMATRIXA16 matProj;
+	D3DXMatrixPerspectiveFovLH(&matProj, D3DX_PI / 2, 1.0f, 1.0f, 100.0f);
+	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
+}
+
 //-----------------------------------------------------------------------------
 // Name: Render()
 // Desc: Draws the scene
@@ -687,6 +730,12 @@ VOID Render()
 		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 10);
 
 		SetupMatrices09();
+		// Render the vertex buffer contents
+		g_pd3dDevice->SetStreamSource(0, g_pVB01, 0, sizeof(CUSTOMVERTEX));
+		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 10);
+
+		SetupMatrices10();
 		// Render the vertex buffer contents
 		g_pd3dDevice->SetStreamSource(0, g_pVB01, 0, sizeof(CUSTOMVERTEX));
 		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
